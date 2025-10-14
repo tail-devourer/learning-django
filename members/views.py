256072bb2fd283acc25c29925b57a35f7d663491
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2025-10-14 09:54:39 -0500

    Added code to display all members stored in the database

--- members/views.py
+++ members/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def list_members(request):
+    members = Member.objects.all().values()
     template = loader.get_template('list_members.htm')
-    return HttpResponse(template.render())
+
+    context = {
+        'members': members,
+    }
+
+    return HttpResponse(template.render(context, request))
