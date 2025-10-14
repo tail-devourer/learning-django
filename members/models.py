94ce263ab2f2328045affd3cc8ec46da8b67bbef
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2025-10-14 09:49:50 -0500

    Added phone and joined_date fields to the Member model

--- members/models.py
+++ members/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Member(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
