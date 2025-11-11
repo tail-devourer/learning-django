bbc41f0bfff64158ef9ad7741f6071d97a68fd3b
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2025-11-11 15:35:17 -0600

    Added comment section

--- members/views.py
+++ members/views.py
@@ -1,10 +1,21 @@
 from django.db.models import Q
-from django.shortcuts import render
-from .models import Member
+from django.shortcuts import render, redirect
+from .models import Member, Comment
+from .forms import CommentForm
 
 def home(request):
     query = request.GET.get('q', '')
     members = None
+    form = None
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+            return redirect('home')
+    else:
+        form = CommentForm()
 
     if query:
         members = Member.objects.filter(
@@ -17,5 +28,7 @@ def home(request):
 
     return render(request, 'list_members.html', {
         'members': members,
+        'comments': Comment.objects.all(),
+        'form': form,
         'query': query,
     })
