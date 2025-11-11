11beb436d2cc80c30bcde870dae741a3b1079266
Sanjangeet Singh <sanjangeetsingh97@gmail.com>
2025-11-11 13:28:56 -0600

    Added search functionality

--- members/views.py
+++ members/views.py
@@ -1,7 +1,21 @@
+from django.db.models import Q
 from django.shortcuts import render
 from .models import Member
 
 def home(request):
+    query = request.GET.get('q', '')
+    members = None
+
+    if query:
+        members = Member.objects.filter(
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(phone__icontains=query)
+        )
+    else:
+        members = Member.objects.all()
+
     return render(request, 'list_members.html', {
-        'members': Member.objects.all(),
+        'members': members,
+        'query': query,
     })
